8eb6fa726113c48e017590b22f8293470bafbf1a
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-03-29 21:51:56 +0200

    Sys::ProcTitle::Versionable introduced

---
+++ src/lib/sys/proc_title/concerns/versionable.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'pathname'
+require 'active_support/concern'
+require 'active_support/inflector'
+require 'version_info'
+
+# Provides a standardized way to use ``VersionInfo``
+#
+# Define ``VERSION_PATH_LEVELS`` in order to suit your needs
+module Sys::ProcTitle::Versionable
+  extend ActiveSupport::Concern
+
+  included do
+    VERSION_PATH_LEVELS = 2 unless const_defined?(:VERSION_PATH_LEVELS)
+    version_info
+  end
+
+  module ClassMethods
+    def version_info
+      unless const_defined?(:VERSION)
+        include VersionInfo
+
+        VersionInfo.file_format = :yaml
+        self.VERSION.file_name = version_filepath
+        self.VERSION.load
+      end
+
+      self.VERSION.to_hash.freeze
+    end
+
+    protected
+
+    # Get path to the ``version`` file
+    #
+    # @return [Pathname]
+    def version_filepath
+      name = ActiveSupport::Inflector.underscore(self.name)
+      dirs = ['..'] * self::VERSION_PATH_LEVELS
+
+      Pathname.new(__dir__).join(*(dirs + [name, 'version_info.yml'])).realpath
+    end
+  end
+end
