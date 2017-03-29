7167904b547f22732c4f26412b7248eaa32b0a69
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-03-29 21:52:29 +0200

    Sys::ProcTitle use Versionable

--- src/lib/sys/proc_title.rb
+++ src/lib/sys/proc_title.rb
@@ -1,40 +1,14 @@
 # frozen_string_literal: true
 
-require 'pathname'
-require 'active_support/inflector'
-require 'version_info'
+require 'pp'
 
 # The Sys module serves as a namespace only
 module Sys
 end
 
 class Sys::ProcTitle
-  class << self
-    # @return [Hash]
-    def version_info
-      unless self.const_defined?(:VERSION)
-        include VersionInfo
+  require 'sys/proc_title/concerns/versionable'
 
-        VersionInfo.file_format = :yaml
-        VERSION.file_name = self.version_filepath
-        VERSION.load
-      end
-
-      VERSION.to_hash.freeze
-    end
-
-    protected
-
-    # Get path to the ``version`` file
-    #
-    # @return [Pathname]
-    def version_filepath
-      name = ActiveSupport::Inflector.underscore(self.name)
-
-      Pathname.new(__dir__).join('..', name, 'version_info.yml').realpath
-    end
-  end
-
-  # registers version_info
-  self.version_info
+  VERSION_PATH_LEVELS = 3
+  include Versionable
 end
