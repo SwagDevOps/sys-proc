877abf807ea78d4d184ab7cf6d891f446329b463
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-04-07 21:54:46 +0200

    sys/proc (spec) 12 examples

--- spec/sys/proc_spec.rb
+++ spec/sys/proc_spec.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
+# class methods
 describe Sys::Proc do
-  describe 'class methods' do
-    methods = [:version_info, :instance, 'VERSION']
-
-    methods.each do |method|
-      it { expect(described_class).to respond_to(method) }
-    end
+  [:version_info, :instance, 'VERSION'].each do |method|
+    it { expect(described_class).to respond_to(method) }
   end
 
-  describe 'instance methods' do
-    methods = [:pid, :name, 'name=']
+  it { expect(described_class).to define_constant('VERSION') }
+end
 
-    methods.each do |method|
-      it { expect(described_class).to respond_to(method) }
+# instance methods are available as class methods
+[Sys::Proc.instance, Sys::Proc].each do |subject|
+  describe subject do
+    [:pid, :name, 'name=', :host_os].each do |method|
+      it { expect(subject).to respond_to(method) }
+    end
+  end
+end
+
+# operating system contexts
+describe Sys::Proc do
+  if :linux_gnu == described_class.host_os
+    context 'when %s' % described_class.host_os do
+      # @todo
     end
   end
 end
