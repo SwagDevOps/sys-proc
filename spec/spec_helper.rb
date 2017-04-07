98b518f980c288604afbadfbfe4f3c3163d0f794
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-04-07 08:38:56 +0200

    spec_helper (spec) introduced

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+# Sample of use:
+#
+# ~~~~
+# it { expect(described_class).to define_constant(constant) }
+# ~~~~
+RSpec::Matchers.define :define_constant do |const|
+  match do |owner|
+    owner.const_defined?(const)
+  end
+end
