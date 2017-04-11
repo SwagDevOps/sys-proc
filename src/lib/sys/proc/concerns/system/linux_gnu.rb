89e3a5b401c25e2495116b0e07a77c024da7854a
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-04-11 08:44:38 +0200

    system/linux_gnu (concerns) implements real methods

--- src/lib/sys/proc/concerns/system/linux_gnu.rb
+++ src/lib/sys/proc/concerns/system/linux_gnu.rb
@@ -6,15 +6,32 @@ require 'sys/proc/concerns/system'
 module Sys::Proc::Concerns::System::LinuxGnu
   extend ActiveSupport::Concern
 
-  protected
+  # Set process title
+  #
+  # @param [String] title
+  # @return [String]
+  def title=(title)
+    title = title.to_s
 
-  # @todo implement
-  def set_proc_name(name)
-    name
+    prctl.set_name(title)
+    $PROGRAM_NAME = title
+
+    title
   end
 
-  # @todo implement
-  def get_proc_name
-    nil
+  # Get process title
+  #
+  # @return [String]
+  def title
+    prctl.get_name
+  end
+
+  protected
+
+  # @return [Sys::Proc::Os::LinuxGnu::Prctl]
+  def prctl
+    require 'sys/proc/system/linux_gnu/prctl'
+
+    Sys::Proc::System::LinuxGnu::Prctl.new
   end
 end
