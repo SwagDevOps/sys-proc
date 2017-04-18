aff16ce03e8b7065959e5b1bf562ca7bb522d333
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-04-18 19:39:35 +0200

    loader provides a better pp (development)

--- src/lib/sys-proc.rb
+++ src/lib/sys-proc.rb
@@ -1,8 +1,18 @@
+# rubocop:disable Style/FileName
 # frozen_string_literal: true
+# rubocop:enable Style/FileName
 
-require 'active_support/inflector'
 require 'pathname'
-
 $LOAD_PATH.unshift Pathname.new(__dir__)
+
+if 'development' == ENV['PROJECT_MODE']
+  require 'pp'
+  require 'coderay'
+  require 'pry/color_printer'
+
+  def pp(obj, out=STDOUT, width=79)
+    (out.isatty ? Pry::ColorPrinter : PP).pp(obj, out, width)
+  end
+end
 
 require 'sys/proc'
