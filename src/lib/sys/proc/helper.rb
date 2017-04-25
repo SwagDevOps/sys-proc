7b2f93f24528d3e281ae0a8485dcd04450c4e875
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-04-25 08:33:00 +0200

    helper (lib) passed to Singleton + uncluttering

--- src/lib/sys/proc/helper.rb
+++ src/lib/sys/proc/helper.rb
@@ -4,7 +4,9 @@ require 'sys/proc'
 
 # Provides access to helper classes
 class Sys::Proc::Helper
-  def initialize
+  include ::Singleton
+
+  protected def initialize
     @items = {
       inflector: proc do
         require 'sys/proc/helper/inflector'
@@ -12,41 +14,37 @@ class Sys::Proc::Helper
         Inflector.new
       end.call
     }
+
+    super
   end
 
   # @param [String|Symbol] name
   # @return [Object]
+  #
+  # @raise [NotImplementedError]
   def get(name)
     name = name.to_sym
 
     return items[name] if items[name]
 
-    @items[name] = inflector.load("sys/proc/helper/#{name}").new
+    begin
+      @items[name] = inflector.load("sys/proc/helper/#{name}").new
+    rescue LoadError
+      raise NotImplementedError, "helper not loadable: #{name}"
+    end
   end
 
   protected
 
   attr_reader :items
 
-  # @return [Sys::Proc::Helper::Inflector]
-  def inflector
-    items.fetch(:inflector)
+  # @return [Hash]
+  def to_h
+    items
   end
 
-  class << self
-    # Provides access to instance methods
-    def method_missing(method, *args, &block)
-      if respond_to_missing?(method)
-        self.new.public_send(method, *args, &block)
-      else
-        super
-      end
-    end
-
-    def respond_to_missing?(method, include_private = false)
-      return true if self.new.respond_to?(method, include_private)
-
-      super(method, include_private)
-    end
+  # @return [Sys::Proc::Helper::Inflector]
+  def inflector
+    to_h.fetch(:inflector)
   end
 end
