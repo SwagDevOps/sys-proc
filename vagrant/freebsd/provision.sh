7cad79297803980aca9e93d0a943865ba42c6ff1
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-05-01 17:58:12 +0200

    freebsd (vagrant) major changes and improvements

--- vagrant/freebsd/provision.sh
+++ vagrant/freebsd/provision.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
-pkg install -y bash bash-completion htop curl vim direnv
+for i in bash bash bash-completion htop curl vim-lite direnv; do
+    pkg install -y "$i"
+done
 # bash setup
 chsh -s '/usr/local/bin/bash' 'vagrant'
 echo '. /etc/profile 2>/dev/null' > /home/vagrant/.profile
 echo '. "${HOME}/.bashrc" 2>/dev/null' >> /home/vagrant/.profile
-echo 'gem: --no-ri --no-rdoc' > /home/vagrant/.gemrc
+for i in /home/vagrant /root; do
+    echo 'gem: --no-ri --no-rdoc' > "${i}/.gemrc"
+done
 
 chown -Rf 'vagrant:vagrant' '/home/vagrant'
-
 rm -rf '/home/vagrant/VBoxGuestAdditions.iso'
 # RVM installation
-export RUBY_VERSION=2.3
+export RUBY_VERSION=2.3.3
 test -f '/usr/local/rvm/bin/rvm' || {
     curl -sSL 'https://get.rvm.io' | bash -s 'stable'
 }
 
-bash -c "(. /etc/profile.d/rvm.sh && rvm use ${RUBY_VERSION})" || {
-    bash -c "(. /etc/profile.d/rvm.sh && rvm install ${RUBY_VERSION})"
-    bash -c "(. /etc/profile.d/rvm.sh && rvm use ${RUBY_VERSION})"
+bash << RVM_INSTALL
+. /etc/profile.d/rvm.sh
+export BUNDLE_SILENCE_ROOT_WARNING=1
+
+rvm use "${RUBY_VERSION}" || {
+    rvm install "${RUBY_VERSION}"
+    rvm use "${RUBY_VERSION}"
 }
 
-bash -c "(. /etc/profile.d/rvm.sh && gem install --conservative bundler)"
-ln -sfv '/usr/local/rvm/rubies/ruby-2.3.3/bin/ruby' "/usr/local/bin/ruby${RUBY_VERSION}"
+gem install --conservative bundler
+(cd /vagrant && {
+    bundle install
+    chown -Rf 'vagrant:vagrant' vendor/*
+})
+RVM_INSTALL
+
+ln -sfv \
+   "/usr/local/rvm/rubies/ruby-${RUBY_VERSION}/bin/ruby" \
+   "/usr/local/bin/ruby$(echo ${RUBY_VERSION} | perl -pe 's#([0-9]+)\.([0-9]+)\.([0-9]+)$#$1.$2#')"
