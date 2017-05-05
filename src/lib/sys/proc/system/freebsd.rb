ba303bd544a1f4a956bcd5269c4a73c6df2402d6
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-05-05 08:41:10 +0200

    system/freebsd/lib_c (lib) introduced

---
+++ src/lib/sys/proc/system/freebsd.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'sys/proc/system'
+
+# Freebsd specific namespace
+module Sys::Proc::System::Freebsd
+
+end
---
+++ src/lib/sys/proc/system/freebsd/lib_c.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'fiddle'
+require 'pathname'
+
+require 'sys/proc/system/freebsd'
+
+# @see http://man7.org/linux/man-pages/man2/prctl.2.html
+class Sys::Proc::System::Freebsd::LibC
+  # TODO populate :)
+end
