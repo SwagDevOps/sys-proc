cee6510b7e4d3144b94e068d977ad4e2e00b7b25
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-05-07 14:55:14 +0200

    sys-proc (lib) missing ``bundler/setup`` added + ``pp`` documentation

--- src/lib/sys-proc.rb
+++ src/lib/sys-proc.rb
@@ -5,15 +5,27 @@
 $LOAD_PATH.unshift __dir__
 
 if 'development' == ENV['PROJECT_MODE']
+  require 'rubygems'
+  require 'bundler/setup'
+
   require 'pp'
   begin
     require 'coderay'
     require 'pry/color_printer'
   rescue LoadError => e
-    warn("%s: %s" % [caller[0], e.message])
+    warn('%s: %s' % [caller[0], e.message])
   end
 
-  def pp(obj, out=STDOUT, width=nil)
+  # Outputs obj to out in pretty printed format of width columns in width.
+  #
+  # If out is omitted, ``STDOUT`` is assumed.
+  # If width is omitted, ``79`` is assumed.
+  #
+  # @param [Object] obj
+  # @param [IO] out
+  # @param [Fixnum] width
+  # @see http://ruby-doc.org/stdlib-2.2.0/libdoc/pp/rdoc/PP.html
+  def pp(obj, out = STDOUT, width = nil)
     args = [obj, out, width].compact
     colorable = (out.isatty and Kernel.const_defined?('Pry::ColorPrinter'))
 
@@ -21,4 +33,4 @@ if 'development' == ENV['PROJECT_MODE']
   end
 end
 
-require File.basename(__FILE__, '.rb').gsub('-', '/')
+require File.basename(__FILE__, '.rb').tr('-', '/')
