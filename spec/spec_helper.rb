8adb10dca53f089798bd796c30e10936631c974a
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-05-08 20:01:36 +0200

    spec_helper (spec) provides Pidof

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,30 @@ module RSpec::DSL
   end
 end
 
+# Find the pid(s) of a process
+#
+# Sample of use:
+#
+# ```
+# Pidof.find(:systemd)
+# ```
+class Pidof
+  class << self
+    def find(progname)
+      require 'open3'
+
+      Open3.capture3(executable, progname.to_s)[0]
+           .chomp.split(/\s+/).map(&:to_i)
+    end
+
+    def executable
+      require 'cliver'
+
+      Cliver.detect!(:pidof)
+    end
+  end
+end
+
 # Sample of use:
 #
 # ~~~~
