cdd015f8d690130ae85bc8c60350b2329665d75e
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-05-22 17:58:09 +0200

    license header added

--- src/lib/sys-proc.rb
+++ src/lib/sys-proc.rb
@@ -2,6 +2,12 @@
 # frozen_string_literal: true
 # rubocop:enable Style/FileName
 
+# Copyright (C) 2017 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
 $LOAD_PATH.unshift __dir__
 
 if 'development' == ENV['PROJECT_MODE']
--- src/lib/sys/proc/system/freebsd.rb
+++ src/lib/sys/proc/system/freebsd.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+# Copyright (C) 2017 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
 require 'sys/proc/system'
 
 # Freebsd specific namespace
