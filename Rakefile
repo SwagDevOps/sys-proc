2ad892afe8b9df0a1e89ee859aaa0b0d6f09b93c
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-12 21:02:00 +0200

    bootstrapping changes

--- Rakefile
+++ Rakefile
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
-require 'rubygems'
-require 'bundler/setup'
+require_relative 'lib/sys-proc'
+require 'sys/proc'
 require 'pathname'
 
-$LOAD_PATH.unshift Pathname.new(__dir__).join('rake', 'lib')
-
+$LOAD_PATH.unshift Pathname.new(__dir__).join('rake/lib')
 require 'project'
 
 Dir.glob('rake/**/*.rake').each {|f| load(f)}
--- lib/sys-proc.rb
+++ lib/sys-proc.rb
@@ -8,45 +8,44 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
-if File.file?("#{__dir__}/../Gemfile.lock")
-  require 'rubygems'
-  require 'bundler'
+$LOAD_PATH.unshift(__dir__)
 
-  Bundler.setup(:default)
+lock = Dir.chdir("#{__dir__}/..") do
+  [['gems.rb', 'gems.locked'], ['Gemfile', 'Gemfile.lock']]
+    .map { |m| Dir.glob(m).size >= 2 }
+    .include?(true)
+end
+mode = (ENV['PROJECT_MODE'] || lock ? 'development' : 'production').to_sym
+
+if lock
+  require 'rubygems'
+  require 'bundler/setup'
 end
 
-$LOAD_PATH.unshift __dir__
+if lock and :development == mode
+  require 'pp'
+  begin
+    require 'coderay'
+    require 'pry/color_printer'
+  rescue LoadError => e
+    # rubocop:disable Performance/Caller
+    warn('%s: %s' % [caller[0], e.message])
+    # rubocop:enable Performance/Caller
+  end
 
-if File.file?("#{__dir__}/../Gemfile.lock")
-  if 'development' == ENV['PROJECT_MODE']
-    require 'bundler/setup'
+  # Outputs obj to out in pretty printed format of width columns in width.
+  #
+  # If out is omitted, ``STDOUT`` is assumed.
+  # If width is omitted, ``79`` is assumed.
+  #
+  # @param [Object] obj
+  # @param [IO] out
+  # @param [Fixnum] width
+  # @see http://ruby-doc.org/stdlib-2.2.0/libdoc/pp/rdoc/PP.html
+  def pp(obj, out = STDOUT, width = 79)
+    args = [obj, out, width].compact
+    colorable = (out.isatty and Kernel.const_defined?('Pry::ColorPrinter'))
 
-    require 'pp'
-    begin
-      require 'coderay'
-      require 'pry/color_printer'
-    rescue LoadError => e
-      # rubocop:disable Performance/Caller
-      warn('%s: %s' % [caller[0], e.message])
-      # rubocop:enable Performance/Caller
-    end
-
-    # Outputs obj to out in pretty printed format of width columns in width.
-    #
-    # If out is omitted, ``STDOUT`` is assumed.
-    # If width is omitted, ``79`` is assumed.
-    #
-    # @param [Object] obj
-    # @param [IO] out
-    # @param [Fixnum] width
-    # @see http://ruby-doc.org/stdlib-2.2.0/libdoc/pp/rdoc/PP.html
-    def pp(obj, out = STDOUT, width = 79)
-      args = [obj, out, width].compact
-      colorable = (out.isatty and Kernel.const_defined?('Pry::ColorPrinter'))
-
-      (colorable ? Pry::ColorPrinter : PP).pp(*args)
-    end
+    (colorable ? Pry::ColorPrinter : PP).pp(*args)
   end
 end
-
-require File.basename(__FILE__, '.rb').tr('-', '/')
