5db202e457413d3a111b916669c16bfae1e8d6ef
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-30 20:07:15 +0200

    update license

--- lib/sys-proc.rb
+++ lib/sys-proc.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 # rubocop:enable Style/FileName
 
-# Copyright (C) 2017 Dimitri Arrigoni <dimitri@arrigoni.me>
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
 # License GPLv3+: GNU GPL version 3 or later
 # <http://www.gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
--- lib/sys/proc/concern.rb
+++ lib/sys/proc/concern.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (C) 2017 Dimitri Arrigoni <dimitri@arrigoni.me>
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
 # License GPLv3+: GNU GPL version 3 or later
 # <http://www.gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
