00c6bc12fedbdf0da8d641aa9a34df2cda5650dc
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-03 13:13:10 +0200

    spec_helper/pidof (spec) minor changes

--- spec/spec_helper/pidof.rb
+++ spec/spec_helper/pidof.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'open3'
+require 'cliver'
+
 # Find the pid(s) of a process
 #
 # Sample of use:
@@ -10,15 +13,11 @@
 class Pidof
   class << self
     def find(progname)
-      require 'open3'
-
       Open3.capture3(executable, progname.to_s)[0]
            .chomp.split(/\s+/).map(&:to_i)
     end
 
     def executable
-      require 'cliver'
-
       Cliver.detect!(:pidof)
     end
   end
