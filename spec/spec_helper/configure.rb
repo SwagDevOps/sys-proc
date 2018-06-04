282118adcecdabeacaafdf5394f472d628a99df2
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-04 21:00:14 +0200

    spec_helper/configure (spec) minor changes

--- spec/spec_helper/configure.rb
+++ spec/spec_helper/configure.rb
@@ -4,6 +4,6 @@ require 'pathname'
 require 'sham'
 require_relative 'local'
 
-Sham::Config.activate!(Pathname.new(__dir__).join('..').realpath.to_path)
+Sham::Config.activate!(Pathname.new(__dir__).join('..').realpath)
 
 Object.class_eval { include Local }
